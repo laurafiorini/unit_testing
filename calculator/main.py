42a600dbcae3335952b472b5c9fdc2aaf8c462e8
Laura Fiorini <laurafiorinib@gmail.com>
2024-03-20 10:42:36 -0300

    reverting main function to its original objective

--- calculator/main.py
+++ calculator/main.py
@@ -7,18 +7,15 @@ def main(request):
 
     try:
         request_json = request.get_json()
-        # num1 = request_json.get('num1')
-        # num2 = request_json.get('num2')
-        # operator = request_json.get('operator')
+        num1 = int(request_json.get('num1'))
+        num2 = int(request_json.get('num2'))
+        operator = request_json.get('operator')
     except Exception as e:
         return json.dumps({'error': f'Erro ao processar o JSON da requisição. {e}'}), 400
 
-    # if not num1 or not num2 or operator is None:
-    #     return json.dumps({'error': 'num1, num2 e operador são obrigatórios'}), 400
+    if not num1 or not num2 or operator is None:
+        return json.dumps({'error': 'num1, num2 e operador são obrigatórios'}), 400
 
-    num1 = 2
-    num2 = 3
-    operator = '^'
     result = calculate(num1, num2, operator)
 
     return json.dumps({'operacao_realizada': f'{num1}{operator}{num2}', 'resultado': result}), 200
